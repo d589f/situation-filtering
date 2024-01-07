a9d1d9e6fa76f34b99f68080034d7dcab83e8037
Dridta <44748644+Dridta@users.noreply.github.com>
2024-01-07 17:35:57 +0200

    new files

---
+++ ListsProject/Cars/Ambulance.java
@@ -0,0 +1,8 @@
+package Cars;
+
+public class Ambulance extends Legacies{
+    public void ambulance(){
+
+        System.out.println("you need ambulance");
+    }
+}
---
+++ ListsProject/Cars/Police.java
@@ -0,0 +1,8 @@
+package Cars;
+
+public class Police extends Legacies{
+    public void police(){
+
+        System.out.println("you need police");
+    }
+}
